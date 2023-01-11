502d3d3e56345823ca48d4d469cbbef3406dc47f
Diego Avila <diegoalonsoag91@gmail.com>
2023-01-10 22:19:12 -0600

    Version stable

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import './App.css';
-import logoFreecodecamp from './images/freecodecamp-logo.png';
+import { ListTasks } from './components/listTasks';
 
 function App() {
   return (
     <div className="App">
-      <div className="container_logo_freecodecamp">
-        <img src={logoFreecodecamp} alt="" className="logo_freecodecamp" />
-      </div>
       <div className="container_taks_list">
         <h1>
           Mis tareas
         </h1>
-        
+        <ListTasks />
       </div>
     </div>
   );
--- src/components/task.jsx
+++ src/components/task.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import '../css-custom/task.css';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
 
-export function Task({text}) {
+export function Task({ id, text, done, checkTask, deleteTask }) {
   return(
-    <div className="container_task">
-      <div className="text_task">
+    <div className={`container_task ${ done ? 'done' : ''}`}>
+      <div className="text_task" onClick={() => checkTask(id)}>
         {text}
       </div>
-      <div className="icon_task">
-        
+      <div className="container_icons_task" onClick={() => deleteTask(id)}>
+        <AiOutlineCloseCircle  className='icon_task'/>
       </div>
     </div>
   )
